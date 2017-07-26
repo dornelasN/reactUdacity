5e90b7f3e95669516606be612c0286d8d0ee8c7d
dornelasN <ndornelasj@gmail.com>
2017-07-26 13:37:04 -0700

    Remove default files, add provided content for icons, index.css, ContatsAPI.js, ImageInput.js, and App.js, cloned the Backend Server into project

--- contacts/src/App.js
+++ contacts/src/App.js
@@ -1,35 +1,34 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import ListContacts  from './ListContacts'
 
-class ContactList extends React.Component{
-
-  render() {
-    const people = this.props.contacts
-
-    //creates Element with JSX
-    return <ol>
-      {people.map(person => (
-        <li key={person.name}>{person.name}</li>
-      ))}
-    </ol>
+const contacts = [
+  {
+    "id": "ryan",
+    "name": "Ryan Florence",
+    "email": "ryan@reacttraining.com",
+    "avatarURL": "http://localhost:5001/ryan.jpg"
+  },
+  {
+    "id": "michael",
+    "name": "Michael Jackson",
+    "email": "michael@reacttraining.com",
+    "avatarURL": "http://localhost:5001/michael.jpg"
+  },
+  {
+    "id": "tyler",
+    "name": "Tyler McGinnis",
+    "email": "tyler@reacttraining.com",
+    "avatarURL": "http://localhost:5001/tyler.jpg"
   }
-}
+]
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <ContactList contacts={[
-          { name : 'Michael'},
-          { name : 'Moon'},
-          { name : 'Nelson'}
-        ]}/>
-        <ContactList contacts={[
-          { name : 'Amanda'},
-          { name : 'Jeff'},
-          { name : 'Mariane'}
-        ]}/>
+      <div>
+        <ListContacts contacts={contacts}/>
       </div>
-    );
+    )
   }
 }
 
