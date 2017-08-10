f89d9e370c7368c9b44514ee455f5eed7b10d442
dornelasN <ndornelasj@gmail.com>
2017-08-10 11:55:54 -0700

    Link now matches two different links, exact "/" and "/create" and <Link> allows browser to back to previous history path

--- contacts/src/index.js
+++ contacts/src/index.js
@@ -8,4 +8,6 @@ import registerServiceWorker from './registerServiceWorker';
 ReactDOM.render(
   <BrowserRouter><App /></BrowserRouter>,
   document.getElementById('root')
-)
+);
+
+registerServiceWorker();
